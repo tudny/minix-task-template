3fb5e0693e7a7dda8b7d42e9e2d3a5315e74f6bb
Tudny <aleksanderwt@gmail.com>
2022-05-08 17:32:57 +0200

    Update start_machine.sh

--- scripts/start_machine.sh
+++ scripts/start_machine.sh
@@ -13,7 +13,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . "${dir}/.config"
 
 # Required commands.
-commands=("qemu-system-x86_64")
+commands=("qemu-system-x86_64 ssh")
 for c in "${commands[@]}"
 do
 if ! command -v "${c}" &> /dev/null
@@ -25,3 +25,8 @@ done
 cd "${dir}/../images" || fail "could not cd to /images dir"
 
 qemu-system-x86_64 -curses -enable-kvm -drive file=minix.img -rtc base=localtime -net user,hostfwd=tcp::"${ssh_port}"-:22 -net nic,model=virtio -m 1024M
+
+echo "setting the timezone"
+current_time=$(date "+%C%y%m%d%H%M.%S")
+ssh root@localhost -p "${ssh_port}" "echo export TZ=Europe/Warsaw > /etc/rc.timezone"
+ssh root@localhost -p "${ssh_port}" "date ${current_time}"
